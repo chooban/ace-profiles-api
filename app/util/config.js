02c06528a1bbe923546fff321a2ceb5695a5bb08
Ross Hendry <rhendry@googlemail.com>
2017-03-17 12:00:26 +0000

    Removed forever to let docker deal with restarts

--- app/util/config.js
+++ app/util/config.js
@@ -13,6 +13,7 @@ if (process.env.SECRET) {
 } else {
   config.secret = fs.readFileSync('/run/secrets/auth0_client_secret', 'utf8').trim();
   config.audience = fs.readFileSync('/run/secrets/auth0_client_id', 'utf8').trim();
+  config.managementToken = fs.readFileSync('/run/secrets/auth0_management_token', 'utf8').trim();
 }
 
 module.exports = config;
