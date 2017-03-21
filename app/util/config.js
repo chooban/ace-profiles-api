968742f317e92b9bf79d9b6f97f58e04f0410786
Ross Hendry <rhendry@googlemail.com>
2017-03-21 21:25:08 +0000

    Get domain from secrets

--- app/util/config.js
+++ app/util/config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const config = {
-  domain: 'acemyorder.eu.auth0.com',
+  domain: '',
   secret: '',
   audience: '',
   managementToken: ''
@@ -10,10 +10,13 @@ const config = {
 if (process.env.SECRET) {
   config.secret = process.env.SECRET;
   config.audience = process.env.AUDIENCE;
+  config.managementToken = process.env.MANAGEMENT;
+  config.domain = process.env.DOMAIN;
 } else {
   config.secret = fs.readFileSync('/run/secrets/auth0_client_secret', 'utf8').trim();
   config.audience = fs.readFileSync('/run/secrets/auth0_client_id', 'utf8').trim();
   config.managementToken = fs.readFileSync('/run/secrets/auth0_management_token', 'utf8').trim();
+  config.domain = fs.readFileSync('/run/secrets/auth0_domain', 'utf8').trim();
 }
 
 module.exports = config;
